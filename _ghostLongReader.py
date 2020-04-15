4b52cc117805258ce73df0c54b5d61e792e0359d
noopy <young_ahn@yonsei.ac.kr>
2020-04-15 13:40:52 +0900

    update ghostLongReader

--- _ghostLongReader.py
+++ _ghostLongReader.py
@@ -95,7 +95,7 @@ def main():
 
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "--local_file_path", type=str, default="2000010307-20181228-549.flac"
+        "--local_file_path", type=str, default="transformed_flac/test.flac"
     )
     args = parser.parse_args()
 
