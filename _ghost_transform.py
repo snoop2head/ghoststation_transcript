f0ac8278c0db30afed6ebe6769779510fac35c96
noopy <young_ahn@yonsei.ac.kr>
2020-04-15 13:33:58 +0900

    transform all files in mp3 directory

--- _ghost_transform.py
+++ _ghost_transform.py
@@ -1,16 +1,17 @@
 # from os import path
 from pydub import AudioSegment
+import os
+import glob
 
 
-def transform(file_name_without_format):
+def transform(file_name_without_format, src):
     # files
     # src = r"C:/Users/pc/Desktop/ghostation/z.past_files/" + file_name_without_format + ".mp3"
-    src = (
-        "/Users/noopy/ghoststation_transcript/downloadedmp3/"
+    dst = (
+        "/Users/noopy/ghoststation_transcript/transformed_flac/"
         + file_name_without_format
-        + ".mp3"
+        + ".flac"
     )
-    dst = "/Users/noopy/ghoststation_transcript/" + file_name_without_format + ".flac"
 
     # convert wav to mp3
     sound = AudioSegment.from_mp3(src)
@@ -18,4 +19,19 @@ def transform(file_name_without_format):
     print("done")
 
 
-transform("2000010307-20181228-549")
+def move_transform():
+    # change the source directory to your project directory
+    # source = '/path/to/source_folder'
+    source = "/Users/noopy/ghoststation_transcript/downloadedmp3/"
+
+    mp3_file_list = glob.glob(source + "*.mp3")
+    print(mp3_file_list)
+
+    for item in mp3_file_list:
+        print(item)
+        item_name_list = item.split(".")[-2]
+        item_name = item_name_list.split("/")[-1]
+        transform(item_name, item)
+
+
+move_transform()
