c4ef1b4ec6327b0ccf7df8402ab778b9ab9a668c
noopy <young_ahn@yonsei.ac.kr>
2020-04-19 04:13:04 +0900

    STT for FLAC file on cloud storage completed

---
+++ _cloudGhostLongReader.py
@@ -0,0 +1,57 @@
+from google.cloud import speech_v1
+from google.cloud.speech_v1 import enums
+import io
+import os
+
+os.environ[
+    "GOOGLE_APPLICATION_CREDENTIALS"
+] = r"/Users/noopy/ghoststation_transcript/snoop2head.json"
+
+
+def sample_long_running_recognize(storage_uri):
+    """
+    Transcribe long audio file from Cloud Storage using asynchronous speech
+    recognition
+
+    Args:
+      storage_uri URI for audio file in Cloud Storage, e.g. gs://[BUCKET]/[FILE]
+    """
+
+    client = speech_v1.SpeechClient()
+
+    # storage_uri = 'gs://cloud-samples-data/speech/brooklyn_bridge.raw'
+
+    # Sample rate in Hertz of the audio data sent
+    sample_rate_hertz = 44100
+
+    # The language of the supplied audio
+    language_code = "ko-KR"
+
+    audio_channel_count = 2
+    enable_separate_recognition_per_channel = True
+
+    # Encoding of audio data sent. This sample sets this explicitly.
+    # This field is optional for FLAC and WAV audio formats.
+    encoding = enums.RecognitionConfig.AudioEncoding.FLAC
+    config = {
+        "sample_rate_hertz": sample_rate_hertz,
+        "audio_channel_count": audio_channel_count,
+        "enable_separate_recognition_per_channel": enable_separate_recognition_per_channel,
+        "language_code": language_code,
+        "encoding": encoding,
+    }
+    audio = {"uri": storage_uri}
+
+    operation = client.long_running_recognize(config, audio)
+
+    print(u"Waiting for operation to complete...")
+    response = operation.result()
+
+    for result in response.results:
+        # First alternative is the most probable result
+        alternative = result.alternatives[0]
+        print(u"Transcript: {}".format(alternative.transcript))
+
+
+uri = "gs://ghoststation/2000010307-20180831-456.flac"
+sample_long_running_recognize(uri)
